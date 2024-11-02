f9db220d29362d9225ecec332de8c44ab5374618
jaquevan <jaquevan@bu.edu>
2024-11-02 18:11:12 -0400

    added jordans food api

---
+++ components/FoodCard.js
@@ -0,0 +1,40 @@
+"use client"
+import styled from "styled-components";
+
+/**
+
+ FoodData Type
+ @typedef {Object} FoodData
+ @property {string} description - The description of the food item.
+ @property {number} gramWeight - The gram weight of the food item.
+ @property {Array<{nutrientName: string, value: number, unitName: string}>} foodNutrients - Array of nutrients.*/
+
+const FoodCardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 1rem;
+    border: 1px solid black;
+    margin: 1rem;
+    width: 250px;
+    border-radius: 10px;
+    background-color: #f9f9f9;
+`;
+
+export default function NutritionApp({foodNutrients = [] }) {
+    return (
+        <FoodCardWrapper className="food-card">
+            <ul>
+                {foodNutrients.length > 0 ? (
+                    foodNutrients.map((nutrient, index) => (
+                        <li key={index}>
+                            {nutrient.nutrientName}: {nutrient.value} {nutrient.unitName}
+                        </li>
+                    ))
+                ) : (
+                    <p>No nutritional information available.</p>
+                )}
+            </ul>
+        </FoodCardWrapper>
+    );
+}
--- components/sideNav.js
+++ components/sideNav.js
@@ -7,163 +7,167 @@ import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
 import Sheet from '@mui/joy/Sheet';
 import Apps from '@mui/icons-material/Apps';
-import Settings from '@mui/icons-material/Settings';
-import Person from '@mui/icons-material/Person';
 import Dropdown from '@mui/joy/Dropdown';
 import MenuButton from '@mui/joy/MenuButton';
-
-// The Menu is built on top of Popper v2, so it accepts `modifiers` prop that will be passed to the Popper.
-// https://popper.js.org/docs/v2/modifiers/offset/
+import Link from 'next/link';
 
 const modifiers = [
-  {
-    name: 'offset',
-    options: {
-      offset: ({ placement }) => {
-        if (placement.includes('end')) {
-          return [8, 20];
-        }
-        return [-8, 20];
-      },
+    {
+        name: 'offset',
+        options: {
+            offset: ({ placement }) => {
+                if (placement.includes('end')) {
+                    return [8, 20];
+                }
+                return [-8, 20];
+            },
+        },
     },
-  },
 ];
 
 function NavMenuButton({
-  children,
-  menu,
-  open,
-  onOpen,
-  onLeaveMenu,
-  label,
-  ...props
-}) {
-  const isOnButton = React.useRef(false);
-  const internalOpen = React.useRef(open);
+                           children,
+                           menu,
+                           open,
+                           onOpen,
+                           onLeaveMenu,
+                           label,
+                           ...props
+                       }) {
+    const isOnButton = React.useRef(false);
+    const internalOpen = React.useRef(open);
 
-  const handleButtonKeyDown = (event) => {
-    internalOpen.current = open;
-    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
-      event.preventDefault();
-      onOpen(event);
-    }
-  };
-
-  return (
-    <Dropdown
-      open={open}
-      onOpenChange={(_, isOpen) => {
-        if (isOpen) {
-          onOpen?.();
+    const handleButtonKeyDown = (event) => {
+        internalOpen.current = open;
+        if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+            event.preventDefault();
+            onOpen(event);
         }
-      }}
-    >
-      <MenuButton
-        {...props}
-        slots={{ root: IconButton }}
-        slotProps={{ root: { variant: 'plain', color: 'neutral' } }}
-        onMouseDown={() => {
-          internalOpen.current = open;
-        }}
-        onClick={() => {
-          if (!internalOpen.current) {
-            onOpen();
-          }
-        }}
-        onMouseEnter={() => {
-          onOpen();
-          isOnButton.current = true;
-        }}
-        onMouseLeave={() => {
-          isOnButton.current = false;
-        }}
-        onKeyDown={handleButtonKeyDown}
-        sx={[
-          {
-            '&:focus-visible': {
-              bgcolor: 'neutral.plainHoverBg',
-            },
-          },
-          open ? { bgcolor: 'neutral.plainHoverBg' } : { bgcolor: null },
-        ]}
-      >
-        {children}
-      </MenuButton>
-      {React.cloneElement(menu, {
-        onMouseLeave: () => {
-          onLeaveMenu(() => isOnButton.current);
-        },
-        modifiers,
-        slotProps: {
-          listbox: {
-            id: `nav-example-menu-${label}`,
-            'aria-label': label,
-          },
-        },
-        placement: 'right-start',
-        sx: {
-          width: 288,
-          [`& .${menuClasses.listbox}`]: {
-            '--List-padding': 'var(--ListDivider-gap)',
-          },
-        },
-      })}
-    </Dropdown>
-  );
+    };
+
+    return (
+        <Dropdown
+            open={open}
+            onOpenChange={(_, isOpen) => {
+                if (isOpen) {
+                    onOpen?.();
+                }
+            }}
+        >
+            <MenuButton
+                {...props}
+                slots={{ root: IconButton }}
+                slotProps={{ root: { variant: 'plain', color: 'neutral' } }}
+                onMouseDown={() => {
+                    internalOpen.current = open;
+                }}
+                onClick={() => {
+                    if (!internalOpen.current) {
+                        onOpen();
+                    }
+                }}
+                onMouseEnter={() => {
+                    onOpen();
+                    isOnButton.current = true;
+                }}
+                onMouseLeave={() => {
+                    isOnButton.current = false;
+                }}
+                onKeyDown={handleButtonKeyDown}
+                sx={[
+                    {
+                        '&:focus-visible': {
+                            bgcolor: 'neutral.plainHoverBg',
+                        },
+                    },
+                    open ? { bgcolor: 'neutral.plainHoverBg' } : { bgcolor: null },
+                ]}
+            >
+                {children}
+            </MenuButton>
+            {React.cloneElement(menu, {
+                onMouseLeave: () => {
+                    onLeaveMenu(() => isOnButton.current);
+                },
+                modifiers,
+                slotProps: {
+                    listbox: {
+                        id: `nav-example-menu-${label}`,
+                        'aria-label': label,
+                    },
+                },
+                placement: 'right-start',
+                sx: {
+                    width: 288,
+                    [`& .${menuClasses.listbox}`]: {
+                        '--List-padding': 'var(--ListDivider-gap)',
+                    },
+                },
+            })}
+        </Dropdown>
+    );
 }
 
 NavMenuButton.propTypes = {
-  children: PropTypes.node,
-  label: PropTypes.string.isRequired,
-  menu: PropTypes.element.isRequired,
-  onLeaveMenu: PropTypes.func.isRequired,
-  onOpen: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
+    children: PropTypes.node,
+    label: PropTypes.string.isRequired,
+    menu: PropTypes.element.isRequired,
+    onLeaveMenu: PropTypes.func.isRequired,
+    onOpen: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
 };
 
 export default function SideNav() {
-  const [menuIndex, setMenuIndex] = React.useState(null);
-  const itemProps = {
-    onClick: () => setMenuIndex(null),
-  };
-  const createHandleLeaveMenu = (index) => (getIsOnButton) => {
-    setTimeout(() => {
-      const isOnButton = getIsOnButton();
-      if (!isOnButton) {
-        setMenuIndex((latestIndex) => {
-          if (index === latestIndex) {
-            return null;
-          }
-          return latestIndex;
-        });
-      }
-    }, 200);
-  };
-  return (
-    <Sheet sx={{ width: "50%", border: "2px solid white", borderRadius: 'sm', py: 20, bgcolor: "black", color: "white"}}>
-      <List>
-        <ListItem>
-          <NavMenuButton
-            label="Apps"
-            open={menuIndex === 0}
-            onOpen={() => setMenuIndex(0)}
-            onLeaveMenu={createHandleLeaveMenu(0)}
-            menu={
-              <Menu onClose={() => setMenuIndex(null)}>
-                <MenuItem {...itemProps}>Meditation</MenuItem>
-                <MenuItem {...itemProps}>Productivity Tracker</MenuItem>
-                <MenuItem {...itemProps}>Fitness Data</MenuItem>
-                <MenuItem {...itemProps}>Nutrition Calculator</MenuItem>
-              </Menu>
-            }
-          >
-            <Apps />
-          </NavMenuButton>
-        </ListItem>
-        <ListItem>
+    const [menuIndex, setMenuIndex] = React.useState(null);
+    const itemProps = {
+        onClick: () => setMenuIndex(null),
+    };
 
-        </ListItem>
-      </List>
-    </Sheet>
-  );
+    const createHandleLeaveMenu = (index) => (getIsOnButton) => {
+        setTimeout(() => {
+            const isOnButton = getIsOnButton();
+            if (!isOnButton) {
+                setMenuIndex((latestIndex) => {
+                    if (index === latestIndex) {
+                        return null;
+                    }
+                    return latestIndex;
+                });
+            }
+        }, 200);
+    };
+
+    return (
+        <Sheet sx={{ width: "50%", border: "2px solid white", borderRadius: 'sm', py: 20, bgcolor: "black", color: "white"}}>
+            <List>
+                <ListItem>
+                    <NavMenuButton
+                        label="Apps"
+                        open={menuIndex === 0}
+                        onOpen={() => setMenuIndex(0)}
+                        onLeaveMenu={createHandleLeaveMenu(0)}
+                        menu={
+                            <Menu onClose={() => setMenuIndex(null)}>
+                                <MenuItem {...itemProps}>
+                                    <Link href="/foodcard" passHref>Food Card</Link>
+                                </MenuItem>
+                                <MenuItem {...itemProps}>
+                                    <Link href="/home" passHref>Productivity Tracker</Link>
+                                </MenuItem>
+                                {/*<MenuItem {...itemProps}>*/}
+                                {/*    <Link href="/FitnessData" passHref>Fitness Data</Link>*/}
+                                {/*</MenuItem>*/}
+                                {/*<MenuItem {...itemProps}>*/}
+                                {/*    <Link href="/NutritionCalculator" passHref>Nutrition Calculator</Link>*/}
+                                {/*</MenuItem>*/}
+                            </Menu>
+                        }
+                    >
+                        <Apps />
+                    </NavMenuButton>
+                </ListItem>
+                <ListItem></ListItem>
+            </List>
+        </Sheet>
+    );
 }
