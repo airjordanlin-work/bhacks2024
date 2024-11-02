330968faada759f715429047fbd5400b758411e2
jaquevan <jaquevan@bu.edu>
2024-11-02 19:51:44 -0400

    revert jordan changes and add buddy component

---
+++ components/Buddy.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const StyledDiv = styled.div`
+    border: 2 px solid grey;
+
+`
+
+export default function BuddyTerminal(){
+    return (
+        <StyledDiv>
+            <img/>
+        </StyledDiv>
+    )
+}
