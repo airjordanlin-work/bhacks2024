87907fabd600ebd4409414e5ef68930bfdd6215e
Davbkim0420@gmail.com <Davbkim0420@gmail.com>
2024-11-02 18:31:24 -0400

    added journal routing

---
+++ app/journal/page.jsx
@@ -0,0 +1,11 @@
+import CustomStepper from "@/components/stepper";
+
+export default function JournalPage() {
+    return (
+        <>
+            <h1>Journal Page</h1>
+            <h2>Yo</h2>
+
+        </>
+    );
+}
